920d668a0717e894889045935d1ed1b3e9ab2005
Leonardo <leonardouchoa.pedreira@gmail.com>
2021-11-01 18:28:24 -0300

    Add geolocation filtering in  step

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -33,6 +33,10 @@ def go(args):
     logger.info("Convert last_review feature to datetime")
     df['last_review'] = pd.to_datetime(df['last_review'])
 
+    logger.info("Drop rows in the dataset that are not in the proper geolocation")
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
     filename = args.output_artifact
     df.to_csv(filename, index=False)
 
